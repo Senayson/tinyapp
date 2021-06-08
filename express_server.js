aa7fc645dea2e3fa8d4fe0de4035293db4dc5be0
Senayson <senaialema@gmail.com>
2021-06-08 17:56:41 +0000

    Created /new route for POST request

--- express_server.js
+++ express_server.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
+
+function generateRandomString() {
+
+}
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -22,6 +29,14 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase};
   res.render("urls_index", templateVars)
  });
+ app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
  app.get("/urls/:shortURL", (req, res) => {
    const longURL = urlDatabase[req.params.shortURL];
@@ -29,6 +44,7 @@ app.get("/urls", (req, res) => {
    console.log(req.params);
   res.render("urls_show", templateVars)
  });
+
  
  
 app.listen(PORT, () => {
